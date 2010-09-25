bd198ed964a7d5371c1fa454a334649bda295008
Sami Kantoluoto <sami.kantoluoto@embedtronics.fi>
2010-09-25 15:55:01 +0300

    gpiolib - fixed gpio_set_values() to ignore unallocated pins

--- drivers/gpio/gpiolib.c
+++ drivers/gpio/gpiolib.c
@@ -1125,6 +1125,14 @@ int gpio_set_values(unsigned gpio, unsigned count, const int *values)
 
 	while (count > 0) {
 		struct gpio_chip *chip = gpio_to_chip(gpio);
+
+		if (!chip) {
+			gpio++;
+			values++;
+			count--;
+			continue;
+		}
+
 		unsigned offs = gpio - chip->base;
 		unsigned num = chip->ngpio - offs;
 
