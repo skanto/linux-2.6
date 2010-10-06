6e7befb687916f00451bb8fa420e6356d8e56511
Sami Kantoluoto <sami.kantoluoto@embedtronics.fi>
2010-10-06 17:43:39 +0300

    TM-EFDC-gpio - added ioctl() interface

--- drivers/gpio/tm_efdc-gpio.c
+++ drivers/gpio/tm_efdc-gpio.c
@@ -26,9 +26,9 @@
 #include <linux/hrtimer.h>
 #include <linux/kthread.h>
 #include <linux/wait.h>
-#include <linux/vmalloc.h>
 #include <linux/freezer.h>
 #include <linux/poll.h>
+#include <linux/mutex.h>
 
 #include <asm/io.h>
 #include <mach/hardware.h>
@@ -42,7 +42,7 @@
 #define	ADC_SLIDE_WIN		16		// length of ADC sliding window
 #define	LOAD_DET_INTERVAL	5000		// open-load detection interval (in ms)
 
-struct efdcgpio_dev {
+static struct efdcgpio_dev {
 	struct hrtimer		timer;		// timer for constant polling
 	struct task_struct	*kthread;	// kthread for polling
 
@@ -50,9 +50,13 @@ struct efdcgpio_dev {
 
 	unsigned		configured:1;	// set if device has been configured
 
+	int			load_det_tick;	// load float detection
+
 	u32			di_last;	// last state of digital inputs
 	u32			do_raw;		// last transmitted do_raw
 	int			di_delay[TM_EFDC_DIN_COUNT];	// current delay value for inputs
+
+	u64			do_float;	// mask of floating outputs
 
 	u32			pwr_last;	// last state of power bits
 	int			pwr_delay[2];	// delay value for power bits
@@ -65,10 +69,13 @@ struct efdcgpio_dev {
 
   	u32			adc_slide[TM_EFDC_AIN_COUNT];	// sliding window..
 } efdcgpio_dev = {
+	.configured		= 0,
 };
 
 static const volatile u16 pwr_on_delay[2] = { 500, 500 };
 static const volatile u16 pwr_off_delay[2] = { 500, 500 };
+
+static DEFINE_MUTEX(efdc_gpio_mutex);
 
 #if TM_EFDC_DOUT_PWM_NBITS == 32
 static const u32 pwm_32_table[33] = {
@@ -131,10 +138,14 @@ static u32 tm_efdc_gpio_in_filter(struct efdcgpio_dev *dev, int count, u32 raw, 
   return new_state;
 }
 
+#define	EFDC_PIN_LOAD_DET	AT91_PIN_PC0
+#define	EFDC_PIN_DO_FLAG	AT91_PIN_PC4
+
 static void tm_efdc_gpio_dio_polled(void *context, u32 di_raw, u32 do_stat)
 {
   struct efdcgpio_dev *dev = context;
   u32 di_new;
+  int ldet;
 
   // combine...
   di_raw ^= dev->gpio->DI_Invert;			// apply invert
@@ -146,6 +157,46 @@ static void tm_efdc_gpio_dio_polled(void *context, u32 di_raw, u32 do_stat)
   di_new = tm_efdc_gpio_in_filter(dev, TM_EFDC_DIN_COUNT, di_raw, dev->di_last,
 				  dev->di_delay, dev->gpio->DI_OnDelay, dev->gpio->DI_OffDelay);
   dev->gpio->DI = dev->di_last = di_new;
+
+  // was load state detection active:
+  ldet = at91_get_gpio_value(EFDC_PIN_LOAD_DET);
+  if (!ldet) {						// load detection was active:
+    dev->do_float = 0;					// so nothing floats by default..
+  }
+
+  // process digital output errors:
+  if (!at91_get_gpio_value(EFDC_PIN_DO_FLAG)) {		// some kind of error:
+    int c;
+    u64 err, err1;
+    u32 do_fail = do_stat ^ dev->do_raw;		// mask of failed outputs
+
+    err = dev->do_float;				// current errors
+
+    err1 = !ldet ? 0x02 : 0x01;				// error 1 (0x01 = shortcut, 0x02 = floating)
+
+    c = 0; do {
+      if (do_fail & (1U << c)) {			// this output has failure:
+	err = ((err & ~(3ULL << (c * 2)))		// merge error bits:
+	       | ((do_stat & (1U << c) ? err1 : 0x01LLU) << (c * 2)));
+      }
+    } while (++c < TM_EFDC_DOUT_COUNT);
+
+    dev->gpio->DO_Status = err;				// show to application
+
+    if (!ldet) {					// load detection was active:
+      dev->do_float = err & 0xAAAAAAAAAAAAAAAALLU;	// save floating outputs
+    }
+  } else {						// no errors:
+    dev->gpio->DO_Status = dev->do_float;		// remember floating outputs
+  }
+
+  // is it time to do load floating detection:
+  if (--dev->load_det_tick <= 0) {
+    dev->load_det_tick = LOAD_DET_INTERVAL;
+    at91_set_gpio_value(EFDC_PIN_LOAD_DET, 0);
+  } else {
+    at91_set_gpio_value(EFDC_PIN_LOAD_DET, 1);
+  }
 }
 
 static __inline void poll_dio(struct efdcgpio_dev *dev)
@@ -232,16 +283,19 @@ static int tm_efdc_gpio_kthread(void *data)
 static enum hrtimer_restart tm_efdc_gpio_timer_cb(struct hrtimer *tmr)
 {
 	struct efdcgpio_dev *dev = &efdcgpio_dev;
+	u32 portb, portc;
 
 	// get switches and power status:
-	u32 portb = at91_sys_read(AT91_PIOB + PIO_PDSR);	// read port B inputs
-	u32 portc = at91_sys_read(AT91_PIOC + PIO_PDSR);	// read port C inputs
+	portb = at91_sys_read(AT91_PIOB + PIO_PDSR);	// read port B inputs
+	portc = at91_sys_read(AT91_PIOC + PIO_PDSR);	// read port C inputs
 
 	hrtimer_add_expires_ns(tmr, POLL_INTERVAL);
 
 	// poll digital I/O
 	if (dev->gpio->Configured || dev->configured) {
-		dev->configured = 1;
+		if (!dev->configured) {
+			dev->configured = 1;
+		}
 		poll_dio(dev);
 	}
 
@@ -297,17 +351,217 @@ static int tm_efdc_gpio_close(struct inode *inode, struct file *file)
 /*
  * Handle commands from user-space.
  */
-static long tm_efdc_gpio_ioctl(struct file *file,
+static __inline long _tm_efdc_gpio_ioctl(struct file *file,
 					unsigned int cmd, unsigned long arg)
 {
-	//void __user *argp = (void __user *)arg;
+	tm_efdc_gpio_t *gpio = efdcgpio_dev.gpio;
+	void __user *argp = (void __user *)arg;
+	tm_efdc_gpio_set_t set;
+	tm_efdc_gpio_set_2_t set2;
+	__u8 u8[32];
+	__u32 u32[32];
+	__u16 u16[32];
+	__u64 u64;
+	int i;
 	//int __user *p = argp;
 	//int new_value, err;
 
 	switch (cmd) {
+	case EFDCIOC_GET_DI:
+		u32[0] = gpio->DI;
+		return copy_to_user(argp, &u32, 4) ? -EFAULT : 0;
+
+	case EFDCIOC_GET_DI_RAW:
+		u32[0] = gpio->DI_Raw;
+		return copy_to_user(argp, &u32, 4) ? -EFAULT : 0;
+
+	case EFDCIOC_GET_DI_INV:
+		u32[0] = gpio->DI_Invert;
+		return copy_to_user(argp, &u32, 4) ? -EFAULT : 0;
+
+	case EFDCIOC_SET_DI_INV:
+#if 1
+		if (copy_from_user(argp, &u32, 4))
+			return -EFAULT;
+		gpio->DI_Invert = u32[0];
+		return 0;
+#else
+		if (copy_from_user(argp, &set, sizeof(set)))
+			return -EFAULT;
+		if (set.num < 0 || set.num >= TM_EFDC_DIN_COUNT || set.value < 0 || set.value > 1)
+			return -EINVAL;
+		if (set.value)
+			gpio->DI_Invert |= (1U << set.num);
+		else
+			gpio->DI_Invert &= ~(1U << set.num);
+		return 0;
+#endif
+
+	case EFDCIOC_GET_DI_PULSES:
+		for (i = 0; i < TM_EFDC_DIN_COUNT; i++) {
+			u32[i] = gpio->DI_Pulses[i];
+		}
+		return copy_to_user(argp, &u32, 4 * i) ? -EFAULT : 0;
+
+	case EFDCIOC_GET_DI_ONDELAY:
+		for (i = 0; i < TM_EFDC_DIN_COUNT; i++) {
+			u16[i] = gpio->DI_OnDelay[i];
+		}
+		return copy_to_user(argp, &u16, 2 * i) ? -EFAULT : 0;
+
+	case EFDCIOC_GET_DI_OFFDELAY:
+		for (i = 0; i < TM_EFDC_DIN_COUNT; i++) {
+			u16[i] = gpio->DI_OffDelay[i];
+		}
+		return copy_to_user(argp, &u16, 2 * i) ? -EFAULT : 0;
+
+	case EFDCIOC_SET_DI_ONDELAY:
+	case EFDCIOC_SET_DI_OFFDELAY:
+		if (copy_from_user(argp, &set, sizeof(set)))
+			return -EFAULT;
+		if (set.num < 0 || set.num >= TM_EFDC_DIN_COUNT || set.value < 0 || set.value > 65535)
+			return -EINVAL;
+		if (cmd == EFDCIOC_SET_DI_ONDELAY)
+			gpio->DI_OnDelay[set.num] = set.value;
+		else
+			gpio->DI_OffDelay[set.num] = set.value;
+		return 0;
+
+	case EFDCIOC_GET_ID:
+		u16[0] = gpio->ID;
+		return copy_to_user(argp, &u16, 2) ? -EFAULT : 0;
+
+	case EFDCIOC_GET_POWER:
+		u16[0] = gpio->Power;
+		return copy_to_user(argp, &u16, 2) ? -EFAULT : 0;
+
+	case EFDCIOC_GET_DO:
+		u32[0] = gpio->DO;
+		return copy_to_user(argp, &u32, 4) ? -EFAULT : 0;
+
+	case EFDCIOC_GET_DO_STATUS:
+		u64 = gpio->DO_Status;
+		return copy_to_user(argp, &u64, 8) ? -EFAULT : 0;
+
+	case EFDCIOC_SET_DO:
+		if (copy_from_user(argp, &set, sizeof(set)))
+			return -EFAULT;
+		if (set.num < 0 || set.num >= TM_EFDC_DOUT_COUNT || set.value < 0 || set.value > 1)
+			return -EINVAL;
+		if (set.value)
+			gpio->DO |= (1U << set.num);
+		else
+			gpio->DO &= ~(1U << set.num);
+		return 0;
+
+	case EFDCIOC_SET_DO_2:
+		if (copy_from_user(argp, &set2, sizeof(set2)))
+			return -EFAULT;
+		gpio->DO = (gpio->DO & ~set2.mask) | (set2.states & set2.mask);
+		return 0;
+
+	case EFDCIOC_GET_DO_RAW:
+		u32[0] = gpio->DO_Raw;
+		return copy_to_user(argp, &u32, 4) ? -EFAULT : 0;
+
+	case EFDCIOC_GET_DO_PWM_ENA:
+		u32[0] = gpio->DO_PWM_Ena;
+		return copy_to_user(argp, &u32, 4) ? -EFAULT : 0;
+
+	case EFDCIOC_SET_DO_PWM_ENA:
+		if (copy_from_user(argp, &set, sizeof(set)))
+			return -EFAULT;
+		if (set.num < 0 || set.num >= TM_EFDC_DIN_COUNT || set.value < 0 || set.value > 1)
+			return -EINVAL;
+		if (set.value)
+			gpio->DO_PWM_Ena |= (1U << set.num);
+		else
+			gpio->DO_PWM_Ena &= ~(1U << set.num);
+		return 0;
+
+	case EFDCIOC_SET_DO_PWM_ENA_2:
+		if (copy_from_user(argp, &set2, sizeof(set2)))
+			return -EFAULT;
+		gpio->DO_PWM_Ena = (gpio->DO_PWM_Ena & ~set2.mask) | (set2.states & set2.mask);
+		return 0;
+
+	case EFDCIOC_GET_DO_PWM:
+		if (copy_from_user(argp, &set, sizeof(set)))
+			return -EFAULT;
+		if (set.num < 0 || set.num >= TM_EFDC_DOUT_COUNT)
+			return -EINVAL;
+		set.value = gpio->DO_PWM[set.num];
+		return copy_to_user(argp, &set, sizeof(set)) ? -EFAULT : 0;
+
+	case EFDCIOC_GET_DO_PWM_MIN:
+		for (i = 0; i < TM_EFDC_DOUT_COUNT; i++) {
+			u8[i] = gpio->DO_PWM_Min[i];
+		}
+		return copy_to_user(argp, &u8, i) ? -EFAULT : 0;
+
+	case EFDCIOC_SET_DO_PWM:
+	case EFDCIOC_SET_DO_PWM_MIN:
+		if (copy_from_user(argp, &set, sizeof(set)))
+			return -EFAULT;
+		if (set.num < 0 || set.num >= TM_EFDC_DOUT_COUNT || set.value < 0 || set.value > 100)
+			return -EINVAL;
+		if (cmd == EFDCIOC_SET_DO_PWM)
+			gpio->DO_PWM[set.num] = set.value;
+		else
+			gpio->DO_PWM_Min[set.num] = set.value;
+		return 0;
+
+	case EFDCIOC_GET_AO:
+		if (copy_from_user(argp, &set, sizeof(set)))
+			return -EFAULT;
+		if (set.num < 0 || set.num >= TM_EFDC_AOUT_COUNT)
+			return -EINVAL;
+		set.value = gpio->AO[set.num];
+		return copy_to_user(argp, &set, sizeof(set)) ? -EFAULT : 0;
+
+	case EFDCIOC_SET_AO:
+		if (copy_from_user(argp, &set, sizeof(set)))
+			return -EFAULT;
+		if (set.num < 0 || set.num >= TM_EFDC_AOUT_COUNT || set.value < 0 || set.value > 65535)
+			return -EINVAL;
+		gpio->AO[set.num] = set.value;
+		return 0;
+
+	case EFDCIOC_GET_AI:
+		if (copy_from_user(argp, &set, sizeof(set)))
+			return -EFAULT;
+		if (set.num < 0 || set.num >= TM_EFDC_AIN_COUNT)
+			return -EINVAL;
+		set.value = gpio->AI[set.num];
+		return copy_to_user(argp, &set, sizeof(set)) ? -EFAULT : 0;
+
+	case EFDCIOC_GET_CONFIGURED:
+		i = gpio->Configured;
+		return copy_to_user(argp, &i, sizeof(i)) ? -EFAULT : 0;
+
+	case EFDCIOC_SET_CONFIGURED:
+		if (copy_from_user(argp, &i, sizeof(i)))
+			return -EFAULT;
+		gpio->Configured = i != 0;
+		return 0;
+
 	default:
 		return -ENOTTY;
 	}
+}
+
+static long tm_efdc_gpio_ioctl(struct file *file,
+					unsigned int cmd, unsigned long arg)
+{
+	long ret;
+
+	mutex_lock(&efdc_gpio_mutex);
+
+	ret = _tm_efdc_gpio_ioctl(file, cmd, arg);
+
+	mutex_unlock(&efdc_gpio_mutex);
+
+	return ret;
 }
 
 
@@ -334,29 +588,22 @@ static ssize_t tm_efdc_gpio_write(struct file *file, const char *data,
 
 static int tm_efdc_gpio_mmap(struct file *file, struct vm_area_struct *vma)
 {
-	unsigned long start = vma->vm_start;
 	unsigned long size = vma->vm_end - vma->vm_start;
 	unsigned long offset = vma->vm_pgoff << PAGE_SHIFT;
-	unsigned long page, pos;
+	int ret;
 
-	if (offset + size > PAGE_ALIGN(sizeof(*efdcgpio_dev.gpio))) {
+	if ((offset + size) > PAGE_ALIGN(sizeof(*efdcgpio_dev.gpio))) {
 		return -EINVAL;
 	}
 
-	pos = (unsigned long)efdcgpio_dev.gpio;
+	ret = remap_pfn_range(vma,
+			      vma->vm_start,
+			      virt_to_phys((void*)efdcgpio_dev.gpio) >> PAGE_SHIFT,
+			      size,
+			      vma->vm_page_prot);
 
-	while (size > 0) {
-		page = vmalloc_to_pfn((void *)pos);
-		if (remap_pfn_range(vma, start, page, PAGE_SIZE, PAGE_SHARED)) {
-			return -EAGAIN;
-		}
-		start += PAGE_SIZE;
-		pos += PAGE_SIZE;
-		if (size > PAGE_SIZE)
-			size -= PAGE_SIZE;
-		else
-			size = 0;
-	}
+	if (ret < 0)
+		return ret;
 
 	vma->vm_flags |= VM_RESERVED;   /* avoid to swap out this VMA */
 
@@ -484,7 +731,8 @@ static int request_gpios(struct device *dev)
 
 static int __devinit tm_efdc_gpio_probe(struct platform_device *pdev)
 {
-	unsigned long size, adr;
+	unsigned long size;
+	void *adr;
 	int res, c;
 
 	if (tm_efdc_gpio_miscdev.parent) {
@@ -497,14 +745,13 @@ static int __devinit tm_efdc_gpio_probe(struct platform_device *pdev)
 	if (!efdcgpio_dev.gpio) {
 		// allocate memory for shared memory:
 		size = PAGE_ALIGN(sizeof(*efdcgpio_dev.gpio));
-		efdcgpio_dev.gpio = vmalloc_32(size);
+		efdcgpio_dev.gpio = kmalloc(size, GFP_KERNEL);
 		if (!efdcgpio_dev.gpio) {
 			printk(KERN_ERR DRIVER_NAME ": memory allocation failed\n");
 			return -ENOMEM;
 		}
 
-		// clear memory:
-		memset((void*)efdcgpio_dev.gpio, 0, size);
+		memset((void*)efdcgpio_dev.gpio, 0, sizeof(*efdcgpio_dev.gpio));
 
 		// initialize parameters:
 		for (c = 0; c < TM_EFDC_DIN_COUNT; c++) {
@@ -512,9 +759,9 @@ static int __devinit tm_efdc_gpio_probe(struct platform_device *pdev)
 			efdcgpio_dev.gpio->DI_OffDelay[c] = 20;
 		}
 
-		adr = (unsigned long)efdcgpio_dev.gpio;
+		adr = (void*)efdcgpio_dev.gpio;
 		while (size > 0) {
-			SetPageReserved(vmalloc_to_page((void *)adr));
+			SetPageReserved(virt_to_page(adr));
 			adr += PAGE_SIZE;
 			size -= PAGE_SIZE;
 		}
@@ -533,8 +780,12 @@ static int __devinit tm_efdc_gpio_probe(struct platform_device *pdev)
 		return res;
 	}
 
+	// configure I/O-pins
+	at91_set_gpio_output(EFDC_PIN_LOAD_DET, 1);
+	at91_set_gpio_input(EFDC_PIN_DO_FLAG, 1);
+
 	// prepare hrtimer:
-	hrtimer_init(&efdcgpio_dev.timer, CLOCK_MONOTONIC, HRTIMER_MODE_REL);
+	hrtimer_init(&efdcgpio_dev.timer, CLOCK_MONOTONIC, HRTIMER_MODE_ABS);
 	efdcgpio_dev.timer.function = tm_efdc_gpio_timer_cb;
 
 	// create kthread:
@@ -548,6 +799,8 @@ static int __devinit tm_efdc_gpio_probe(struct platform_device *pdev)
 
 	// start ticking
 	hrtimer_start(&efdcgpio_dev.timer, ktime_set(0, POLL_INTERVAL), HRTIMER_MODE_REL);
+
+	printk(KERN_INFO DRIVER_NAME ": started succesfully\n");
 
 	return 0;
 }
--- include/linux/tm_efdc-gpio.h
+++ include/linux/tm_efdc-gpio.h
@@ -1,6 +1,8 @@
 #ifndef	_tm_efdc_gpio_h_
 #define	_tm_efdc_gpio_h_
 
+#include <linux/ioctl.h>
+
 // structure for TM-EFDC memory mapped I/O:
 
 #define	TM_EFDC_DIN_COUNT	32		// amount of digital inputs
@@ -26,6 +28,7 @@ typedef volatile struct tm_efdc_gpio {
 						//            corresponding DO_PWM_Ena bit is set
   __u32		DO_Raw;				// raw state of digital outputs (R/)
   __u32		DO_PWM_Ena;			// Bit for each mask of digital outputs with PWM enabled (R/W)
+  __u64		DO_Status;			// status of outputs
   __u8		DO_PWM[TM_EFDC_DOUT_COUNT];	// PWM-values (percent) (R)
   __u8		DO_PWM_Set[TM_EFDC_DOUT_COUNT];	// PWM value to set (percent) (W)
   __u8		DO_PWM_Min[TM_EFDC_DOUT_COUNT];	// minimum PWM-values (percent) (R/W)
@@ -33,7 +36,52 @@ typedef volatile struct tm_efdc_gpio {
   __s16		AI[TM_EFDC_AIN_COUNT];		// Analog Input values (raw values but filtered)
 
   __u32		PollCount;			// Counter gets incremented every poll
-  __u32		Configured:1;			// Application must set this when configured
+  __u8		Configured;			// Application must set this when configured
 } tm_efdc_gpio_t;
+
+// I/O control interface:
+#define	EFDC_IOCTL_BASE	'E'
+
+typedef struct tm_efdc_gpio_set {
+	int	num;		// input/output number
+	long	value;		// value
+} tm_efdc_gpio_set_t;
+
+typedef struct tm_efdc_gpio_set_2 {
+	__u32	mask;		// mask
+	__u32	states;		// values
+} tm_efdc_gpio_set_2_t;
+
+#define	EFDCIOC_GET_DI		_IOR(EFDC_IOCTL_BASE, 0, __u32)
+#define	EFDCIOC_GET_DI_RAW	_IOR(EFDC_IOCTL_BASE, 1, __u32)
+#define	EFDCIOC_GET_DI_INV	_IOR(EFDC_IOCTL_BASE, 2, __u32)
+#define	EFDCIOC_SET_DI_INV	_IOW(EFDC_IOCTL_BASE, 3, __u32)
+#define	EFDCIOC_GET_DI_PULSES	_IOR(EFDC_IOCTL_BASE, 4, __u32[TM_EFDC_DIN_COUNT])
+#define	EFDCIOC_GET_DI_ONDELAY	_IOR(EFDC_IOCTL_BASE, 5, __u16[TM_EFDC_DIN_COUNT])
+#define	EFDCIOC_SET_DI_ONDELAY	_IOW(EFDC_IOCTL_BASE, 6, struct tm_efdc_gpio_set)
+#define	EFDCIOC_GET_DI_OFFDELAY	_IOR(EFDC_IOCTL_BASE, 7, __u16[TM_EFDC_DIN_COUNT])
+#define	EFDCIOC_SET_DI_OFFDELAY	_IOW(EFDC_IOCTL_BASE, 8, struct tm_efdc_gpio_set)
+#define	EFDCIOC_GET_ID		_IOR(EFDC_IOCTL_BASE, 9, __u16)
+#define	EFDCIOC_GET_POWER	_IOR(EFDC_IOCTL_BASE, 10, __u16)
+
+#define	EFDCIOC_GET_DO		_IOR(EFDC_IOCTL_BASE, 11, __u32)
+#define	EFDCIOC_SET_DO		_IOW(EFDC_IOCTL_BASE, 12, struct tm_efdc_gpio_set)
+#define	EFDCIOC_SET_DO_2	_IOW(EFDC_IOCTL_BASE, 13, struct tm_efdc_gpio_set_2)
+#define	EFDCIOC_GET_DO_RAW	_IOR(EFDC_IOCTL_BASE, 14, __u32)
+#define	EFDCIOC_GET_DO_PWM_ENA	_IOR(EFDC_IOCTL_BASE, 15, __u32)
+#define	EFDCIOC_SET_DO_PWM_ENA	_IOW(EFDC_IOCTL_BASE, 16, struct tm_efdc_gpio_set)
+#define	EFDCIOC_SET_DO_PWM_ENA_2 _IOW(EFDC_IOCTL_BASE, 17, struct tm_efdc_gpio_set_2)
+#define	EFDCIOC_GET_DO_PWM	_IOWR(EFDC_IOCTL_BASE, 18, struct tm_efdc_gpio_set)
+#define	EFDCIOC_SET_DO_PWM	_IOW(EFDC_IOCTL_BASE, 19, struct tm_efdc_gpio_set)
+#define	EFDCIOC_GET_DO_PWM_MIN	_IOR(EFDC_IOCTL_BASE, 20, __u8[TM_EFDC_DOUT_COUNT])
+#define	EFDCIOC_SET_DO_PWM_MIN	_IOW(EFDC_IOCTL_BASE, 21, struct tm_efdc_gpio_set)
+#define	EFDCIOC_GET_DO_STATUS	_IOR(EFDC_IOCTL_BASE, 22, __u64)
+
+#define	EFDCIOC_GET_AO		_IOWR(EFDC_IOCTL_BASE, 23, struct tm_efdc_gpio_set)
+#define	EFDCIOC_SET_AO		_IOW(EFDC_IOCTL_BASE, 24, struct tm_efdc_gpio_set)
+#define	EFDCIOC_GET_AI		_IOWR(EFDC_IOCTL_BASE, 25, struct tm_efdc_gpio_set)
+
+#define	EFDCIOC_GET_CONFIGURED	_IOR(EFDC_IOCTL_BASE, 98, int)
+#define	EFDCIOC_SET_CONFIGURED	_IOW(EFDC_IOCTL_BASE, 99, int)
 
 #endif	// !_tm_efdc_gpio_h_
